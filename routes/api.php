15506bede1a6df9108d66096ad8bad9343241298
unknown <msg.8925@gmail.com>
2024-11-19 17:28:18 +0700

    Added an API route.

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use function Pest\Laravel\json;
+
+Route::get('/user', function (Request $request) {
+    return response()->json(["Name" => "James", "Age" => "34"]);
+});
