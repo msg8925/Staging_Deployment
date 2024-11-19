c25a33c9ce937cac3c8572b64074d30d66c613db
unknown <msg.8925@gmail.com>
2024-11-19 21:28:43 +0700

    Update.

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 use function Pest\Laravel\json;
 
 Route::get('/user', function (Request $request) {
-    return response()->json(["Name" => "James", "Age" => "34"]);
+    return response()->json(["Name" => "Alex", "Age" => "24"]);
 });
