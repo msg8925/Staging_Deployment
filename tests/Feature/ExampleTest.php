bbc7abac3c16367d1cbcf85e8c71eccd01c0e7ea
unknown <msg.8925@gmail.com>
2024-11-24 16:03:00 +0700

    Added User Controller.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,25 +1,66 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 
-    $response->assertStatus(200);
-});
+// uses(RefreshDatabase::class, WithFaker::class)->group('api');
+
+// it('returns a successful response', function () {
+//     $response = $this->get('/');
+
+//     $response->assertStatus(200);
+// });
 
 
-it('Test the user route', function () {
+// it('Test the user route', function () {
     
-    $response = $this->get('/api/user');
+//     $response = $this->get('/api/user');
 
-    $response->assertStatus(200);
-});
+//     $response->assertStatus(200);
+// });
 
 
-it('Test the user route returns correct value', function () {
+// it('Test the user route returns correct value', function () {
     
-    $response = $this->get('/api/user');
+//     $response = $this->get('/api/user');
 
-    $response->assertJson([
-        'Name' => 'Hellen',
-    ]);
+//     $response->assertJson([
+//         'Name' => 'Hellen',
+//     ]);
+// });
+
+
+// it('Test user being correctly fetched from DB', function () {
+    
+//     User::factory(10)->create();
+
+//     $response = DB::table("users")->first();
+
+//     dd($response);
+
+//     // $response = $this->get('/api/user');
+
+//     // $response->assertJson([
+//     //     'Name' => 'Hellen',
+//     // ]);
+// });
+
+
+test('fetch all users', function () {
+
+    // Create 5 random user and store them in the DB
+    User::factory()->count(5)->create();
+
+    // Peform a HTTP GET request on user route
+    $response = $this->getJson('/api/user');
+
+    dd($response);
+
+    // Check HTTP status and ensure 5 users are returned
+    $response->assertStatus(200)
+             ->assertJsonCount(5, 'data');
 });
+
