3a20a421b7dab8dcc0d5a855cf18a556ab3925e2
unknown <msg.8925@gmail.com>
2024-11-24 11:49:04 +0700

    New Feature Test Added.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,3 +5,21 @@ it('returns a successful response', function () {
 
     $response->assertStatus(200);
 });
+
+
+it('Test the user route', function () {
+    
+    $response = $this->get('/api/user');
+
+    $response->assertStatus(200);
+});
+
+
+it('Test the user route returns correct value', function () {
+    
+    $response = $this->get('/api/user');
+
+    $response->assertJson([
+        'Name' => 'Hellen',
+    ]);
+});
